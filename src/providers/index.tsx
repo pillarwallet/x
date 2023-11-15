a15944497990699b3e0a54ed21c8746ee56d9635
poocart <7067483+poocart@users.noreply.github.com>
2023-11-15 12:49:44 +0200

    added global style

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 import LanguageProvider from './LanguageProvider';
 import { defaultTheme } from '../theme';
 
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: ${({ theme }) => theme.font.primary};
+    background: ${({ theme }) => theme.color.background.body};
+    color: ${({ theme }) => theme.color.text.body};
+  }
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <LanguageProvider>
         {children}
       </LanguageProvider>
