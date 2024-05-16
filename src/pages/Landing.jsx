5f0069a99736a178798ed335e279e161330644e2
techashu123 <techashu123@gmail.com>
2024-05-16 10:02:49 +0000

    Fix Linting

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useLocation } from 'react-router-dom';
 import { createGlobalStyle } from 'styled-components';
 
 import '../styles/landing/tailwind.css';
