2340851f214758944ccc11b7fae2383afd38c49d
RanaBug <rana.khy@gmail.com>
2024-08-01 17:02:23 +0100

    minor fix

--- src/apps/pillarx-app/components/EditorialTag/EditorialTag.tsx
+++ src/apps/pillarx-app/components/EditorialTag/EditorialTag.tsx
@@ -16,16 +16,16 @@ const EditorialTag = ({ label, icon, color }: EditorialTagProps) => {
 
     return (
         <div id='editorial-tag' className="flex relative rounded py-1 px-2 w-fit mr-2">
-            <div 
-                className="absolute inset-0 rounded opacity-10" 
+            <div
+                className="absolute inset-0 rounded opacity-10"
                 style={{ backgroundColor: tagColor, zIndex: 5 }}
             />
             <div className="flex relative items-center gap-1" style={{ zIndex: 10 }}>
-                {IconTag && <IconTag data-testid="icon-tag" style={{ width: 16, height: 16, color: tagColor}}/>}
+                {IconTag && <IconTag data-testid="icon-tag" style={{ width: 16, height: 16, color: tagColor }} />}
                 {label && <p className="text-xs" style={{ color: tagColor }}>{label}</p>}
             </div>
         </div>
     );
-}
+};
 
 export default EditorialTag;
