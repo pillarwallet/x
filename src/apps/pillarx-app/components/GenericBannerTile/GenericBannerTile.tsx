b45e19760486705f63cce5d1770bc20b37619f0f
RanaBug <rana.khy@gmail.com>
2024-10-03 16:00:36 +0100

    clickable banner

--- src/apps/pillarx-app/components/GenericBannerTile/GenericBannerTile.tsx
+++ src/apps/pillarx-app/components/GenericBannerTile/GenericBannerTile.tsx
@@ -53,34 +53,36 @@ const GenericBannerTile = ({ data, isDataLoading }: GenericBannerTileProps) => {
       id="generic-banner-tile"
       className={`p-1 flex-col ${!bannerDisplay && 'hidden'}`}
     >
-      <div
-        className="flex flex-col rounded-2xl bg-cover bg-no-repeat bg-center desktop:min-h-[320px] tablet:min-h-[280px] mobile:min-h-[240px]"
-        style={{
-          backgroundImage:
-            bannerDisplay?.backgroundImage &&
-            `url(${bannerDisplay.backgroundImage})`,
-        }}
-      >
-        <div className="mt-32 p-10 tablet:mt-16 mobile:mt-9 mobile:p-4">
-          <p className="text-[45px] font-medium tablet:leading-[67.5px] desktop:leading-[67.5px] mobile:text-xl mobile:leading-[30px]">
-            {bannerDisplay?.title && bannerDisplay.title}
-          </p>
-          <p className="font-medium desktop:text-[22px] tablet:text-[22px] tablet:leading-[33px] desktop:leading-[33px] mobile:text-sm">
-            {bannerDisplay?.subtitle && bannerDisplay.subtitle}
-          </p>
-          {bannerDisplay?.cta?.text && bannerDisplay?.cta?.href && (
-            <button
-              type="button"
-              className="mt-10 mobile:mt-4"
-              onClick={handleOpenBannerLink}
-            >
-              <p className="font-medium bg-container_grey rounded-md py-3 px-5 mobile:text-sm mobile:py-2 mobile:px-4">
-                {bannerDisplay.cta.text && bannerDisplay.cta.text}
-              </p>
-            </button>
-          )}
+      <a href={bannerDisplay?.cta?.href} target="_blank" rel="noreferrer">
+        <div
+          className="flex flex-col rounded-2xl bg-cover bg-no-repeat bg-center desktop:min-h-[320px] tablet:min-h-[280px] mobile:min-h-[240px]"
+          style={{
+            backgroundImage:
+              bannerDisplay?.backgroundImage &&
+              `url(${bannerDisplay.backgroundImage})`,
+          }}
+        >
+          <div className="mt-32 p-10 tablet:mt-16 mobile:mt-9 mobile:p-4">
+            <p className="text-[45px] font-medium tablet:leading-[67.5px] desktop:leading-[67.5px] mobile:text-xl mobile:leading-[30px]">
+              {bannerDisplay?.title && bannerDisplay.title}
+            </p>
+            <p className="font-medium desktop:text-[22px] tablet:text-[22px] tablet:leading-[33px] desktop:leading-[33px] mobile:text-sm">
+              {bannerDisplay?.subtitle && bannerDisplay.subtitle}
+            </p>
+            {bannerDisplay?.cta?.text && bannerDisplay?.cta?.href && (
+              <button
+                type="button"
+                className="mt-10 mobile:mt-4"
+                onClick={handleOpenBannerLink}
+              >
+                <p className="font-medium bg-container_grey rounded-md py-3 px-5 mobile:text-sm mobile:py-2 mobile:px-4">
+                  {bannerDisplay.cta.text && bannerDisplay.cta.text}
+                </p>
+              </button>
+            )}
+          </div>
         </div>
-      </div>
+      </a>
     </TileContainer>
   );
 };
