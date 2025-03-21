a1cf891b384a9f2860c01224e5aa04355f94dc41
RanaBug <rana.khy@gmail.com>
2025-03-21 17:20:10 +0000

    leaderboard improvements

--- src/apps/leaderboard/components/LeaderboardTab/LeaderboardTab.tsx
+++ src/apps/leaderboard/components/LeaderboardTab/LeaderboardTab.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useWalletAddress } from '@etherspot/transaction-kit';
+import { useEffect, useRef, useState } from 'react';
 
 // types
 import {
@@ -18,13 +19,40 @@ type LeaderboardTabProps = {
 
 const LeaderboardTab = ({ data }: LeaderboardTabProps) => {
   const [visibleCount, setVisibleCount] = useState(10);
+  const loadMoreRef = useRef(null);
+  const walletAddress = useWalletAddress();
 
   const handleLoadMore = () => {
     setVisibleCount((prev) => prev + 10);
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          handleLoadMore();
+        }
+      },
+      { threshold: 1.0 }
+    );
+
+    const currentRef = loadMoreRef.current;
+
+    if (currentRef) {
+      observer.observe(currentRef);
+    }
+
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, [visibleCount]);
+
   const visibleData = data.slice(0, visibleCount);
   const isEndOfData = visibleData.length === data.length;
+
+  const myRankData = data.find((result) => result.address === walletAddress);
 
   return (
     <>
@@ -32,9 +60,38 @@ const LeaderboardTab = ({ data }: LeaderboardTabProps) => {
         id="leaderboard-list-data"
         className="flex flex-col bg-container_grey rounded-2xl desktop:p-5 tablet:p-5 mobile:p-2 desktop:pr-10 tablet:pr-10 mobile:pr-4"
       >
+        {myRankData && (
+          <>
+            <div className="flex justify-between">
+              <Body className="text-purple_light">My rank</Body>
+              <Body className="text-purple_light">PX Points</Body>
+            </div>
+            <div className="flex flex-col">
+              <div className="flex justify-between py-5 py-2">
+                <UserInfo
+                  rank={data.indexOf(myRankData) + 1}
+                  walletAddress={myRankData.address}
+                  rankChange={
+                    'rankChange' in myRankData
+                      ? (myRankData.rankChange as LeaderboardRankChange)
+                      : LeaderboardRankChange.NO_CHANGE
+                  }
+                />
+                <div className="flex desktop:gap-3.5 tablet:gap-3.5 mobile:gap-1.5 items-center">
+                  <p className="desktop:text-[22px] tablet:text-[22px] mobile:text-base">
+                    {myRankData.points}
+                  </p>
+                  <BodySmall className="text-purple_light mobile:text-xs">
+                    PX
+                  </BodySmall>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
         <div className="flex justify-between">
           <Body className="text-purple_light">Rankings</Body>
-          <Body className="text-purple_light">PX Points</Body>
+          {!myRankData && <Body className="text-purple_light">PX Points</Body>}
         </div>
         <div className="flex flex-col">
           {visibleData.map((result, index) => (
@@ -66,14 +123,13 @@ const LeaderboardTab = ({ data }: LeaderboardTabProps) => {
         </div>
       </div>
       {!isEndOfData && (
-        <button
-          id="leaderboard-load-more-button"
-          type="button"
+        <div
+          ref={loadMoreRef}
           onClick={handleLoadMore}
           className="border border-purple_light/[.20] py-3 px-7 rounded-3xl w-fit self-center my-9"
         >
-          Load more
-        </button>
+          Load more...
+        </div>
       )}
     </>
   );
