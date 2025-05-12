ab241b3068e1da889a56fa0abfc38bb6ce77d3ab
RanaBug <rana.khy@gmail.com>
2025-05-12 09:59:57 +0100

    remove pnl param for walletportfolio endpoint

--- src/services/pillarXApiWalletPortfolio.ts
+++ src/services/pillarXApiWalletPortfolio.ts
@@ -73,7 +73,6 @@ export const pillarXApiWalletPortfolio = createApi({
               ),
               unlistedAssets: 'true',
               filterSpam: 'true',
-              pnl: 'true',
             },
           },
         };
