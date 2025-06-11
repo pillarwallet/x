b60d39c49c3d6f44eb6722ca2bd15a0be0e0a96d
LeonMorrison-tech <Lalitpancholi1008@gmail.com>
2025-06-11 19:42:38 +0530

    Fixed test cases

--- src/containers/Main.test.tsx
+++ src/containers/Main.test.tsx
@@ -3,6 +3,17 @@ import renderer from 'react-test-renderer';
 // components
 import Main from './Main';
 
+jest.mock('wagmi', () => {
+  const actualWagmi = jest.requireActual('wagmi');
+  return {
+    ...actualWagmi,
+    createConfig: jest.fn(() => ({})),
+    WagmiProvider: ({ children }: any) => <div>{children}</div>,
+    http: jest.fn(),
+    mainnet: { id: 1 },
+  };
+});
+
 describe('<Main />', () => {
   it('renders correctly', () => {
     const tree = renderer.create(<Main />).toJSON();
