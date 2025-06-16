a0a5d78f01c9c56e73e6adac4e218aa40849149c
lkulier <kulierluka34@gmail.com>
2025-06-16 15:09:59 +0400

    faq-app2

---
+++ src/apps/faq-app/index.tsx
@@ -0,0 +1,23 @@
+
+const App = () => {
+
+    return (
+      <div style={{ width: '100%', height: '100vh' }}>
+        <iframe
+          src="https://docs.pillarx.app/faq/frequently-asked-questions-faq"
+          title="PillarX FAQ - Frequently Asked Questions"
+          style={{
+            width: '100%',
+            height: '100%',
+            border: 'none'
+          }}
+          allow="fullscreen"
+        />
+      </div>
+    );
+  };
+  
+// Export the app component;
+
+export default App;
+
