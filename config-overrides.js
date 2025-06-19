590d65214fee39265ef5b6084f41737bfdb326ce
lkulier <kulierluka34@gmail.com>
2025-06-19 17:00:37 +0400

    config override fix

--- config-overrides.js
+++ config-overrides.js
@@ -29,15 +29,15 @@ module.exports = function override(config) {
 
   // Chunking
   
-  //config.optimization.splitChunks = {
-    //cacheGroups: {
-      //vendor: {
-       // test: /[\\/]node_modules[\\/]/,
-        //chunks: 'all',
-        //maxSize: 50000,
-      //},
-    //},
-  //};
+  config.optimization.splitChunks = {
+    cacheGroups: {
+      vendor: {
+       test: /[\\/]node_modules[\\/]/,
+        chunks: 'all',
+        maxSize: 50000,
+      },
+    },
+  };
 
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
