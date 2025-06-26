5e3e5d252290b8c1831ea944ab93621e32f4312a
nikhil kumar <nikhilkumarpps@gmail.com>
2025-06-26 16:10:32 +0530

    adding bsc and gnosis tokens

--- src/apps/pulse/constants/tokens.ts
+++ src/apps/pulse/constants/tokens.ts
@@ -3,6 +3,8 @@ export const STABLE_CURRENCIES = [
     {chainId: 137, address: "0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359"},
     {chainId: 8453, address: "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913"},
     {chainId: 42161, address: "0xaf88d065e77c8cC2239327C5EDb3A432268e5831"},
+    {chainId: 56, address: "0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d"},
+    {chainId: 100, address: "0x2a22f9c3b484c3629090FeED35F17Ff8F88f76F0"}
 ];
 
 export const CHAIN_LOGOS : {[key: string]: string} = {
