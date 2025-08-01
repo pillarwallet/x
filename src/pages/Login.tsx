eb496ad2a0527249e92d8484b6d9370eee998ab8
Ashutosh Singh <techashu123@gmail.com>
2025-08-01 14:38:17 +0530

    Added Plausible Analytics to Login Page Buttons using trackevent

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { useAccount, useConnect, useDisconnect } from 'wagmi';
+import Plausible from 'plausible-tracker';
 
 // components
 import Button from '../components/Button';
@@ -14,6 +15,11 @@ import ConnectionDebug, { DebugInfo } from '../components/ConnectionDebug';
 // images
 import PillarWalletIcon from '../apps/leaderboard/images/pillar-wallet-icon.svg';
 import PillarXLogo from '../assets/images/pillarX_full_white.png';
+
+// Plausible Domain Config
+const { trackEvent } = Plausible({
+  domain: import.meta.env.VITE_PLAUSIBLE_DOMAIN,
+});
 
 const Login = () => {
   const { login, authenticated, user, ready } = usePrivy();
@@ -182,8 +188,8 @@ const Login = () => {
             )
         )}
         <InsideWrapper>
-          <Button onClick={login} $fullWidth className="plausible-event-name=Login+Get+Started">{t`action.getStarted`}</Button>
-          <Button onClick={listenForWalletConnectUri} $last $fullWidth className="plausible-event-name=Login+Connect+Pillar+Wallet">
+          <Button onClick={() => {trackEvent('Login Page Get Started'); login();}} $fullWidth>{t`action.getStarted`}</Button>
+          <Button onClick={() => {trackEvent('Login Page Connect Pillar Wallet'); listenForWalletConnectUri();}} $last $fullWidth>
             <img src={PillarWalletIcon} alt="pillar-wallet-icon" />
             {t`action.connectPillarWallet`}
           </Button>
