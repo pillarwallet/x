fd06d7638bfafa42a9a7c74b0198d3828244ac3d
Ashutosh Singh <techashu123@gmail.com>
2025-09-30 16:48:20 +0530

    Complete Advertising Page & Formated Style

--- src/pages/Advertising.jsx
+++ src/pages/Advertising.jsx
@@ -5,8 +5,7 @@ import { GlobalStyles } from '../components/LandingPage/GlobalStyles';
 import { Footer } from '../components/LandingPage/Footer';
 import { Header } from '../components/LandingPage/Header';
 
-const Marketing = () => {
-
+const Advertising = () => {
   return (
     <>
       {/* Global Styles */}
@@ -16,18 +15,122 @@ const Marketing = () => {
       <Header />
 
       {/* Hero Section */}
-      <section className="developers_hero">
+      <section className="advertising_hero">
         <div className="container">
-          <div className="developers_hero__wrapper">
-            <div className="developers_hero__content">
-              <h1>Effortless Account Abstraction with PillarX</h1>
+          <div className="advertising_hero__wrapper">
+            <div className="advertising_hero__content">
+              <h1>Collaborate with&nbsp;PillarX</h1>
               <p>
-                PillarX offers a complete account abstraction solution,
-                eliminating the complexities of setting up your own
-                infrastructure and account handling. Our system handles all the
-                intricate details, allowing you to focus on what you do best –
-                developing exceptional projects.
+                Connect with 100,000+ Web3-native users <br />
+                <br />
+                <span>
+                  Engage real crypto users through social media campaigns,
+                  in-app placements, and community-driven initiatives.
+                </span>
               </p>
+              <a
+                href="https://forms.gle/7FjSUpgHRXgb3TAR6"
+                target="_blank"
+                rel="noreferrer"
+                className="cta plausible-event-name=Advertising+Submit+Request"
+              >
+                <span>Submit a request</span>
+              </a>
+            </div>
+            <div className="advertising_hero__inapp">
+              <div className="advertising_hero__inapp__content">
+                <h4>Collaboration options</h4>
+                <h2>
+                  In-app banners in <br />
+                  Pillar Wallet & PillarX
+                </h2>
+              </div>
+              <div className="advertising_hero__inapp__images">
+                <div className="advertising_hero__inapp__images__wrapper">
+                  <img src="/public/landing-images/advertising-pillarx.webp" />
+                  <p>PillarX in-app banner</p>
+                </div>
+                <div className="advertising_hero__inapp__images__wrapper">
+                  <img src="/public/landing-images/advertising-pillar.webp" />
+                  <p>
+                    <a
+                      href="https://pillar.fi"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      Pillar Wallet
+                    </a>{' '}
+                    in-app banner
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Feature Section */}
+      <section className="advertising_feature">
+        <div className="container">
+          <div className="advertising_feature__wrapper">
+            <div className="advertising_feature__detail">
+              <div className="advertising_feature__detail__content gradient_border">
+                <h2>
+                  Push notifications <br />
+                  via Pillar Wallet
+                </h2>
+              </div>
+              <div className="advertising_feature__detail__content gradient_border">
+                <h2>
+                  Token competitions
+                  <br /> on X <span>(Twitter)</span>
+                </h2>
+                <div className="advertising_feature__detail__content__icon">
+                  <img src="/landing-images/advertising-icon-1.svg" />
+                </div>
+              </div>
+            </div>
+            <div className="advertising_feature__detail">
+              <div className="advertising_feature__detail__content gradient_border">
+                <h2>
+                  Branded sections in the <br />
+                  Pillar & PillarX Newsletter
+                </h2>
+                <div className="advertising_feature__detail__content__icon">
+                  <img src="/landing-images/advertising-icon-2.svg" />
+                </div>
+              </div>
+              <div className="advertising_feature__detail__content gradient_border">
+                <h2>
+                  Pinned dApps in the <br />
+                  PillarX Web3 App Store
+                </h2>
+                <div className="advertising_feature__detail__content__icon">
+                  <img src="/landing-images/advertising-icon-3.svg" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Contact Section */}
+      <section className="advertising_contact">
+        <div className="container">
+          <div className="advertising_contact__wrapper">
+            <div className="advertising_contact__content">
+              <h2>
+                Looking to launch, scale, or strengthen your presence in Web3?
+                Let’s grow together!
+              </h2>
+              <a
+                href="https://forms.gle/7FjSUpgHRXgb3TAR6"
+                target="_blank"
+                rel="noreferrer"
+                className="cta plausible-event-name=Advertising+Submit+Request"
+              >
+                <span>Submit a request</span>
+              </a>
             </div>
           </div>
         </div>
@@ -39,4 +142,4 @@ const Marketing = () => {
   );
 };
 
-export default Marketing;
+export default Advertising;
