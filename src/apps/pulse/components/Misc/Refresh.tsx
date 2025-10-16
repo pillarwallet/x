989a714a202c2f6ab85f5000cfaa7d23e0503811
Vignesh <vignesh@pillarproject.io>
2025-10-16 16:37:59 +0530

    fixed lint issue

--- src/apps/pulse/components/Misc/Refresh.tsx
+++ src/apps/pulse/components/Misc/Refresh.tsx
@@ -14,10 +14,11 @@ export default function Refresh({
 }: RefreshProps) {
   return (
     <button
-      className={`flex w-[36px] h-[34px] items-center justify-center rounded-[8px] bg-[#1E1D24] ${disabled || isLoading
-        ? 'cursor-not-allowed opacity-50'
-        : 'cursor-pointer'
-        }`}
+      className={`flex w-[36px] h-[34px] items-center justify-center rounded-[8px] bg-[#1E1D24] ${
+        disabled || isLoading
+          ? 'cursor-not-allowed opacity-50'
+          : 'cursor-pointer'
+      }`}
       type="button"
       aria-label="Refresh"
       onClick={onClick}
