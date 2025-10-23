cd55e7d98edd918be9b05b2c076029b2b330fc15
Ashutosh Singh <techashu123@gmail.com>
2025-10-23 17:58:44 +0530

    Added discalimer in footer

--- src/components/LandingPage/Footer/index.jsx
+++ src/components/LandingPage/Footer/index.jsx
@@ -8,33 +8,51 @@ const Footer = () => {
       <footer className="footer">
         <div className="container">
           <div className="footer__wrapper">
-            <div className="footer__links">
-              <Link to="/privacy-policy">Privacy Policy</Link>
+            <div className="footer__disclaimer">
+              <p>
+                The content on this website is for informational purposes only
+                and does not constitute financial or investment advice.
+                Cryptocurrency assets are volatile and carry market risk. Always
+                do your own research and seek professional guidance before
+                making investment decisions.
+              </p>
             </div>
-            <div className="footer__socials">
-              <a href="https://x.com/PX_Web3" target="_blank" rel="noreferrer">
-                <img src="https://cdn.pillarx.app/social-x.svg" alt="x-image" />
-              </a>
-              <a
-                href="https://t.me/+WmZpena91_dlNDhk"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://cdn.pillarx.app/social-telegram.svg"
-                  alt="home-image"
-                />
-              </a>
-              <a
-                href="https://chat.pillar.fi/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img
-                  src="https://cdn.pillarx.app/social-discord.svg"
-                  alt="discord-image"
-                />
-              </a>
+            <div className="footer__links_wrapper">
+              <div className="footer__links">
+                <Link to="/privacy-policy">Privacy Policy</Link>
+              </div>
+              <div className="footer__socials">
+                <a
+                  href="https://x.com/PX_Web3"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://cdn.pillarx.app/social-x.svg"
+                    alt="x-image"
+                  />
+                </a>
+                <a
+                  href="https://t.me/+WmZpena91_dlNDhk"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://cdn.pillarx.app/social-telegram.svg"
+                    alt="home-image"
+                  />
+                </a>
+                <a
+                  href="https://chat.pillar.fi/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src="https://cdn.pillarx.app/social-discord.svg"
+                    alt="discord-image"
+                  />
+                </a>
+              </div>
             </div>
           </div>
         </div>
