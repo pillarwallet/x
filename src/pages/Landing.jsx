0e7143e692a05bb3865a10eeb1cf938f45539f9f
Ashutosh Singh <techashu123@gmail.com>
2025-11-18 17:59:20 +0530

    Configured Email Octopus for Landing Page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,7 +7,6 @@ import '../styles/landing/tailwind.css';
 // components
 import { Header } from '../components/LandingPage/Header';
 import { Footer } from '../components/LandingPage/Footer';
-import { MailChimp } from '../components/LandingPage/MailChimp';
 
 // utils
 
@@ -82,6 +81,22 @@ export default function LandingPage() {
     }
     return undefined;
   }, [navigate]);
+
+  // Email Octopus Subscriber Form
+  useEffect(() => {
+    const container = document.getElementById('octopusForm');
+    if (!container) return;
+    container.innerHTML = '';
+    const script = document.createElement('script');
+    script.src =
+      'https://eomail5.com/form/6b7992da-c46a-11f0-9bf4-5919083e820b.js';
+    script.async = true;
+    script.setAttribute('data-form', '6b7992da-c46a-11f0-9bf4-5919083e820b');
+    container.appendChild(script);
+    return () => {
+      container.innerHTML = '';
+    };
+  }, []);
 
   // Show loading state if authenticating via React Native
   if (isLoadingAuth) {
@@ -300,7 +315,7 @@ export default function LandingPage() {
                 and exclusive announcements!
               </p>
               {/* Form */}
-              <MailChimp />
+              <div id="octopusForm" class="home_app__subscribe__form"></div>
             </div>
             <div className="home_app__download gradient_border">
               <div className="home_app__download__copy">
